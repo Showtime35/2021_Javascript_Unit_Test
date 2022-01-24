d8a2afdd61b795487c85e8aaf8ceacbdea766667
Mason Shirhall <mason.shirhall@vtsd.com>
2022-01-24 10:07:31 -0500

    losing my sheissen

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,7 +1,12 @@
 /* Put code for the task below */
-function ShowButt(){
-	var button1 = document.getelementbyid("buttonTask1")
-	
+function ShowButt() {
+  var button1 = document.getElementById("buttonTask1");
+  button1.style.display = "block";
 
+}
+
+function byebye() {
+  var button2 = document.getElementById("imageTask1");
+  button2.style.display = "none";
 
 }
