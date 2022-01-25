3b7698d1fc7403924dd14754f0e2793bce0c705e
Mason Shirhall <mason.shirhall@vtsd.com>
2022-01-25 08:54:37 -0500

    test ferda memes

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,18 @@
 /* Put code for the task below */
+function red() {
+    var redbutt = document.getElementById("nametable")
+
+    redbutt.style.backgroundColor = "red"
+}
+
+function blue() {
+    var blackbutt = document.getElementById("nametable")
+
+    blackbutt.style.backgroundColor = "blue"
+
+}
+function yellow() {
+    var yellowbutt = document.getElementById("nametable")
+
+    yellowbutt.style.backgroundColor = "yellow"
+}
